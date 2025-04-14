27f81b3314ea517be4d674d10d4e5cee35cf0f5d
gupby004 <gupby004@mymail.unisa.edu.au>
2025-04-14 15:43:22 +0930

    This is the activity 3

---
+++ car.py
@@ -0,0 +1,13 @@
+class Car:
+    def __init__(self):
+        self.make = "Hyundai"
+        self.model = "i30 N"
+        self.year = 2023
+
+    def display_information(self):
+        print("Make:", self.make)
+        print("Model:", self.model)
+        print("Year:", self.year)
+
+Roadster = Car()
+Roadster.display_information()
